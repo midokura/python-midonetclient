848d1b53c4dd108a68e623cc6d7f8a045765a8c2
Tomoe Sugihara <tomoe@midokura.com>
2012-04-15 10:28:34 +0900

    Remove unneeded path

--- midonet/vifs.py
+++ midonet/vifs.py
@@ -4,8 +4,6 @@ from resource import ResourceBase
 
 class Vif(ResourceBase):
 
-    path = 'vifs'
-
     def create(self, vif_uuid, port_uuid):
         data = {"portId": port_uuid, "id": vif_uuid }
 
