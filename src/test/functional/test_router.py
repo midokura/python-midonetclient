04fcf855f0ddeed5ba8fdd34be5ff95bf5e0514c
Tomoe Sugihara <tomoe@midokura.com>
2012-05-22 23:16:02 +0900

    Add test for updating router
    Signed-off-by: Tomoe Sugihara <tomoe@midokura.com>
    Signed-off-by: Ryu Ishimoto <ryu@midokura.com>

--- src/test/functional/test_router.py
+++ src/test/functional/test_router.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import unittest
+import uuid
 from webob import exc
 
 TOPDIR = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
@@ -12,7 +13,7 @@ TOPDIR = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
 sys.path.insert(0, TOPDIR)
 
 from midonet.client import MidonetClient
-from midonet import utils 
+from midonet import utils
 
 
 class TestRouter(unittest.TestCase):
@@ -41,10 +42,15 @@ class TestRouter(unittest.TestCase):
     def test_list(self):
         self.router.list(self.test_tenant_name)
 
-    def test_create_get_delete(self):
+    def test_create_get_update_delete(self):
         r, c = self.router.create(self.test_tenant_name, self.test_router_name)
         router_uuid = utils.get_uuid(r)
         self.router.get(self.test_tenant_name, router_uuid)
+
+        self.router.update(self.test_tenant_name, router_uuid,'new-name',
+                           inboundFilter=str(uuid.uuid4()),
+                           outboundFilter=str(uuid.uuid4()))
+
         self.router.delete(self.test_tenant_name, router_uuid)
         self.assertRaises(LookupError, self.router.get, self.test_tenant_name, router_uuid)
 
@@ -53,7 +59,7 @@ class TestRouter(unittest.TestCase):
         r2, junk = self.router.create(self.test_tenant_name, "router2")
         r1_uuid = utils.get_uuid(r1)
         r2_uuid = utils.get_uuid(r2)
-        
+
         self.router.link_router_create(self.test_tenant_name, r1_uuid, "10.0.0.0", 30,
                                 "10.0.0.1", "10.0.0.2", r2_uuid)
         self.router.link_router_list(self.test_tenant_name, r1_uuid)
@@ -66,7 +72,7 @@ class TestRouter(unittest.TestCase):
         b, junk = self.bridge.create(self.test_tenant_name, "bridge11")
         r_uuid = utils.get_uuid(r)
         b_uuid = utils.get_uuid(b)
-        
+
         self.router.link_bridge_create(self.test_tenant_name, r_uuid, "10.0.0.0", 24,
                                 "10.0.0.1", b_uuid)
         self.router.link_bridge_list(self.test_tenant_name, r_uuid)
