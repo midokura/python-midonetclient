b08fd9c9770f524ec63d92c11905ab8b2f6ef35f
Tomoe Sugihara <tomoe@midokura.com>
2012-05-31 17:01:28 +0900

    Add constants for port types

---
+++ src/midonet/api.py
@@ -0,0 +1,8 @@
+# Copyright 2012 Midokura Japan KK
+
+class PortType:
+    MATERIALIZED_BRIDGE = "MaterializedBridge";
+    MATERIALIZED_ROUTER = "MaterializedRouter";
+    LOGICAL_BRIDGE = "LogicalBridge";
+    LOGICAL_ROUTER = "LogicalRouter";
+
