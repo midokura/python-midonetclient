80be97ea448b603a27c8b9c783f19fb214a1a7a5
Galo Navarro <galo@midokura.com>
2014-02-06 16:30:05 +0100

    New packaging method
    The new ./package.sh script provides all necessary functionality to
    produce packages without having version numbers in version-controlled
    files.
    
    The debian changelog, and rhel copyright files are now removed and auto
    generate as build products into build/, which is added to the ignore
    file.
    
    Signed-off-by: Galo Navarro <galo@midokura.com>

--- rhel/package.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-version=`grep ^Version rhel/*.spec | cut -d ' ' -f 5`
-package=python-midonetclient
-
-git archive HEAD --prefix=$package-$version/ -o /home/midokura/rpmbuild/SOURCES/$package-$version.tar
-gzip /home/midokura/rpmbuild/SOURCES/$package-$version.tar
-rpmbuild -ba rhel/$package.spec
